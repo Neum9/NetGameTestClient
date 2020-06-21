9d1a141cfe77da2db6aee244d81716977437036b
Neum <1975584042@qq.com>
2020-06-21 22:44:40 +0800

    add some class

--- NetGameTestClient/Program.cs
+++ NetGameTestClient/Program.cs
@@ -10,6 +10,14 @@ namespace NetGameTestClient {
         const int BUFFER_SIZE = 1024;
         Socket socket;
         byte[] readBuff;
+
+        int buffCount = 0;
+        byte[] lenBytes = new byte[sizeof(UInt32)];
+        Int32 msgLength = 0;
+        private string recvStr;
+
+        ProtocolBase proto = new ProtocolBytes();
+
         static void Main(string[] args) {
             //Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             ////Connect
@@ -29,8 +37,18 @@ namespace NetGameTestClient {
 
             Program p = new Program();
             p.Connection();
-            byte[] bytes = System.Text.Encoding.Default.GetBytes("_GET");
-            p.socket.Send(bytes);
+            //byte[] bytes = System.Text.Encoding.Default.GetBytes("_GET");
+            //p.socket.Send(bytes);
+
+
+            ProtocolBytes protocol = new ProtocolBytes();
+            protocol.AddString("HeartBeat");
+            Console.WriteLine("发送" + protocol.GetDesc());
+            p.Send(protocol);
+
+            p.OnLoginClick();
+            p.OnAddClick();
+            p.OnGetClick();
 
             Console.ReadKey();
         }
@@ -50,13 +68,91 @@ namespace NetGameTestClient {
                 //count是接受数据的大小
                 int count = socket.EndReceive(ar);
                 //数据处理
-                string str = System.Text.Encoding.UTF8.GetString(readBuff, 0, count);
-                Console.WriteLine("recv " + str);
+                //string str = System.Text.Encoding.UTF8.GetString(readBuff, 0, count);
+                //Console.WriteLine("recv " + str);
+                buffCount += count;
+                ProcessData();
                 socket.BeginReceive(readBuff, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCb, null);
-            } catch (Exception e) {
+            }
+            catch (Exception e) {
                 Console.WriteLine("连接已断开");
                 socket.Close();
             }
+        }
+
+        private void ProcessData() {
+            //小于长度字节
+            if (buffCount < sizeof(Int32)) {
+                return;
+            }
+            //消息长度
+            Array.Copy(readBuff, lenBytes, sizeof(Int32));
+            msgLength = BitConverter.ToInt32(lenBytes, 0);
+            if (buffCount < msgLength + sizeof(Int32)) {
+                return;
+            }
+            //处理消息
+            //string str = System.Text.Encoding.UTF8.GetString(readBuff, sizeof(Int32), (int)msgLength);
+            ProtocolBase protocol = proto.Decode(readBuff, sizeof(Int32), msgLength);
+            HandleMsg(protocol);
+            //recvStr = str;
+            //清除已经处理的消息
+            int count = buffCount - msgLength - sizeof(Int32);
+            Array.Copy(readBuff, msgLength, readBuff, 0, count);
+            buffCount = count;
+            if (buffCount > 0) {
+                ProcessData();
+            }
+        }
+
+        private void HandleMsg(ProtocolBase protoBase) {
+            ProtocolBytes proto = (ProtocolBytes)protoBase;
+            //获取数值
+            int start = 0;
+            string protoName = proto.GetString(start, ref start);
+            int ret = proto.GetInt(start, ref start);
+            //显示       
+            Console.WriteLine("接收" + proto.GetDesc());
+            recvStr = " 接收 " + proto.GetName() + " " + ret.ToString();
+        }
+
+        private void Send(ProtocolBytes protocol) {
+            //string str = "i am client";
+            //byte[] bytes = System.Text.Encoding.UTF8.GetBytes(str);
+            //byte[] length = BitConverter.GetBytes(bytes.Length);
+            //byte[] sendBuff = length.Concat(bytes).ToArray();
+            //socket.Send(sendBuff);            
+            byte[] bytes = protocol.Encode();
+            byte[] length = BitConverter.GetBytes(bytes.Length);
+            byte[] sendBuff = length.Concat(bytes).ToArray();
+
+            //ProtocolBase pb1 = new ProtocolBytes();
+            //ProtocolBase pb = pb1.Decode(sendBuff, sizeof(Int32), bytes.Length);
+
+            socket.Send(sendBuff);
+        }
+
+        public void OnLoginClick() {
+            ProtocolBytes protocol = new ProtocolBytes();
+            protocol.AddString("Login");
+            protocol.AddString("Cjc");
+            protocol.AddString("123");
+            Console.WriteLine(" 发送 " + protocol.GetDesc());
+            Send(protocol);
+        }
+
+        public void OnAddClick() {
+            ProtocolBytes protocol = new ProtocolBytes();
+            protocol.AddString("AddScore");
+            Console.WriteLine(" 发送 " + protocol.GetDesc());
+            Send(protocol);
+        }
+
+        public void OnGetClick() {
+            ProtocolBytes protocol = new ProtocolBytes();
+            protocol.AddString("GetScore");
+            Console.WriteLine(" 发送 " + protocol.GetDesc());
+            Send(protocol);
         }
     }
 }
